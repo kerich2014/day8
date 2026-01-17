3f040f59b7d48f65f0f40945e842516aaeef080f
Kirill Zolotaryov <252222223a@gmail.com>
2026-01-17 16:14:50 +0300

    n

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -3,7 +3,7 @@ import styles from './Button.module.css'
 const Button = () => {
     return(
         <>
-            <button>Лайк!</button>
+            <button>Лайк! </button>
         </>
     )
 }
